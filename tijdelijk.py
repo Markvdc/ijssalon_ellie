11d1ca90d345e2e21d4d56e87faf8358f63c019a
Mark Van de Casteele <markvandecasteele@skynet.be>
2023-06-07 13:26:47 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,16 @@
+mijn_dictionary = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+aanbieding = mijn_dictionary["aardbei"] * 0.8
+reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter, slechts € {aanbieding}")
+reclame_tekst2 = reclame_tekst[:62]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split(" ")
+for x in reclame_tekst4:
+    el =x.lower()
+    if len(x) > 4:
+        print(x)
+    else:
+        print(el)
